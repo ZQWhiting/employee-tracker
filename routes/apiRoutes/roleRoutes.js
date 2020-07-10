441a9370737eef04aad4d584ec15b5fdcaeffbdb
ZQWhiting <zach.whiting@icloud.com>
2020-07-09 18:12:12 -0600

    added apiRoutes for role table

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -2,4 +2,64 @@ const router = require('express').Router();
 const db = require('../../db/database');
 const inputCheck = require('../../utils/inputCheck');
 
+router.get('/roles', (req, res) => {
+    const sql = `TABLE role`
+    const params = [];
+
+    db.execute(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+router.post('/role', ({body}, res) => {
+
+    const errors = inputCheck(body, 'title', 'salary', 'department_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`
+    const params = [body.title, body.salary, body.department_id]
+
+    db.execute(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: result.insertId
+        });
+    });
+});
+
+router.delete('/role/:id', (req, res) => {
+
+    const sql = `DELETE FROM role WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.execute(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: result.affectedRows
+        });
+    });
+});
+
 module.exports = router;
