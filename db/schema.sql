3af34e2ed46175f5b65aeb6432d18b3d045d542d
ZQWhiting <zach.whiting@icloud.com>
2020-07-09 21:31:36 -0600

    foreign key delete conditions

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DEC NOT NULL,
     department_id INT NOT NULL,
-    CONSTRAINT fk_dep FOREIGN KEY (department_id) REFERENCES department(id),
+    CONSTRAINT fk_dep FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     CONSTRAINT fk_manag FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
     PRIMARY KEY (id)
 );
